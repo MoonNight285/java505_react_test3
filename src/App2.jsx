f37db867cb28783c6e9f72aa4e70426000230119
MoonNight285 <kimjyjh11@naver.com>
2023-01-03 11:48:19 +0900

    하나로 작성되어있던것을 각각의 함수형 컴포넌트로 분리한 예제

---
+++ src/App2.jsx
@@ -0,0 +1,39 @@
+import React from "react"; // 기본으로 설치된 라이브러리는 경로없이 사용가능
+import Contents from "./Contents"; // 직접만든 컴포넌트는 경로 사용해야함
+import Footer from "./Footer";
+import Header from "./Header";
+import Navigate from "./Navigate";
+import PhotoInfo from "./PhotoInfo";
+import LinkList from "./LinkList";
+
+const styles = {
+    fakeImg : {
+        height : 200,
+        backgroundColor : "#aaa"
+    }
+}
+
+function App2() {
+    return (
+        <div>
+            <Header />
+            <Navigate />
+            
+            <div className={"container mt-5"}>
+                <div className={"row"}>
+                    <div className={"col-sm-4"}>
+                        <PhotoInfo />
+                        <LinkList />
+                    </div>
+                    <div className={"col-sm-8"}>
+                        <Contents />
+                        <Contents />
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default App2;
---
+++ src/PhotoInfo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const styles = {
+    fakeImg : {
+        height : 200,
+        backgroundColor : "#aaa"
+    }
+}
+
+function PhotoInfo() {
+    return (
+        <div>
+            <h2>About Me</h2>
+            <h5>Photo of me :</h5>
+            <div style={styles.fakeImg}>Fake Image</div>
+            <p>Some text about me in</p>
+        </div>
+    );
+}
+
+export default PhotoInfo;
